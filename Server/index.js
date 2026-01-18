03a40e7192283e4a5068031ce331f13acaae73ee
deadpool1428 <akshaykadu71@gmail.com>
2026-01-18 18:50:22 +0530

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+/* Middleware */
+app.use(cors());
+app.use(express.json());
+
+/* MongoDB Connection */
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+/* Routes */
+app.use("/api/resume", require("./routes/resumeRoutes"));
+
+/* Health Check */
+app.get("/", (req, res) => {
+  res.send("ATS Resume Builder Backend is running");
+});
+
+/* Server */
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
