58eb7400612817a6bf7d601830ec69f76ca00489
deadpool1428 <akshaykadu71@gmail.com>
2026-01-30 15:33:32 +0530

    Integrate ATSAnalyzer into Builder component
    Added ATSAnalyzer component for improved resume analysis.

--- Frontend/client/src/pages/Builder.js
+++ Frontend/client/src/pages/Builder.js
@@ -1,16 +1,24 @@
 import ResumeForm from "../components/ResumeForm";
 import ResumePreview from "../components/ResumePreview";
-import "../index.css";
+import ATSAnalyzer from "../components/ATSAnalyzer";
 
 export default function Builder() {
   return (
     <div className="builder-container">
       <div className="form-panel">
         <ResumeForm />
+
+        {/* Visual separation between Form & ATS */}
+        <div className="section-divider" />
+        <div style={{ marginTop: "32px" }}>
+          <ATSAnalyzer />
+        </div>
       </div>
+
       <div className="preview-panel">
         <ResumePreview />
       </div>
     </div>
   );
 }
+
