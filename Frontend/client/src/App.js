72eec6c2e86a3130c10c9a0bf063ad30ba01c7e3
deadpool1428 <akshaykadu71@gmail.com>
2026-02-10 16:57:24 +0530

    Refactor App component with routing and context providers

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,11 +1,37 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ResumeProvider } from "./context/ResumeContext";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
 import Builder from "./pages/Builder";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
-    <ResumeProvider>
-      <Builder />
-    </ResumeProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <ResumeProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              <Route
+                path="/builder"
+                element={
+                  <ProtectedRoute>
+                    <Builder />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </ResumeProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
